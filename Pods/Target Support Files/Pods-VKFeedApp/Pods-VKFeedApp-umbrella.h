db274dfa51ae5108133b9a266e622e59fdafa87c
Daniyar Mamadov <mamadovd@gmail.com>
2023-03-03 20:13:30 +0300

    rename xcproject and directory

--- Pods/Target Support Files/Pods-VKFeedApp/Pods-VKFeedApp-umbrella.h
+++ Pods/Target Support Files/Pods-VKFeedApp/Pods-VKFeedApp-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_temp_mailVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_temp_mailVersionString[];
+FOUNDATION_EXPORT double Pods_VKFeedAppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_VKFeedAppVersionString[];
 
