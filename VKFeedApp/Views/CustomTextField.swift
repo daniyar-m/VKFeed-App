ff65d3b9a8e662714e3000bc5ea68c364600ac68
Daniyar Mamadov <mamadovd@gmail.com>
2023-03-10 19:07:31 +0300

    minor bug fix

--- VKFeedApp/Views/CustomTextField.swift
+++ VKFeedApp/Views/CustomTextField.swift
@@ -9,18 +9,31 @@ final class CustomTextField: UITextField {
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = UIColor(red: 0.93, green: 0.93, blue: 0.95, alpha: 1.00)
         placeholder = "Поиск"
-        font = .systemFont(ofSize: 14)
         clearButtonMode = .whileEditing
         borderStyle = .none
         layer.cornerRadius = 8
         clipsToBounds = true
         
-        leftView = UIImageView(image: UIImage(systemName: "magnifyingglass"))
+        leftView = UIImageView(image: UIImage(systemName: "magnifyingglass")?.withTintColor(.lightGray, renderingMode: .alwaysOriginal))
         leftView?.frame = CGRect(x: 0, y: 0, width: 14, height: 14)
         leftViewMode = .always
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 36, dy: 0)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 36, dy: 0)
+    }
+    
+    override func leftViewRect(forBounds bounds: CGRect) -> CGRect {
+        var rect = super.leftViewRect(forBounds: bounds)
+        rect.origin.x += 12
+        return rect
     }
 }
